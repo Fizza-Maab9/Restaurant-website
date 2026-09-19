8bc945d6cbaf4ef90ae91eb5c3c4a21d176994d8
Fizza Maab <maabfizza9@gmail.com>
2026-09-19 10:38:19 +0500

    Update restaurant website

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -47,11 +47,11 @@ const Footer = () => {
             </li>
 
             <li>
-              <Link to="/menu">Menu</Link>
+              <Link to="/about">About</Link>
             </li>
 
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/menu">Menu</Link>
             </li>
 
             <li>
